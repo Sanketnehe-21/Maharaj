d5aed1443efdfd72cf3041e7024a6bff13922206
Sanketnehe-21 <sanketnehe21@gmail.com>
2025-05-26 18:22:26 +0530

    Updating the header with geoonavy animation

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import './App.css'
 import Profile from './Components/Profile'
 import maharaj from './assets/maharaj.png'
 import SplitText from "./ReactBit/SplitText";
-
+import Header from './Components/Header';
 function App() {
 
   const handleAnimationComplete = () => {
     console.log("Letter animation completed!");
   };
   return (
-    <div className="container">
+    <div className="container" style={{ paddingTop: '120px' }}>
+      <Header/>
       <img src={maharaj} alt="Shivaji Maharaj" className="image" />
       <div className="text">
         <Profile />
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,4 +1,6 @@
-import GooeyNav from './GooeyNav'
+import GooeyNav from '../ReactBit/GooeyNav';
+import '../App.css';
+import '../css/Header.css'; // Add this CSS file
 
 const items = [
   { label: "Home", href: "#" },
@@ -6,15 +8,19 @@ const items = [
   { label: "Contact", href: "#" },
 ];
 
-<div style={{ height: '600px', position: 'relative' }}>
-  <GooeyNav
-    items={items}
-    particleCount={15}
-    particleDistances={[90, 10]}
-    particleR={100}
-    initialActiveIndex={0}
-    animationTime={600}
-    timeVariance={300}
-    colors={[1, 2, 3, 1, 2, 3, 1, 4]}
-  />
-</div>
+export default function Header() {
+  return (
+    <header className="main-header">
+      <GooeyNav
+        items={items}
+        particleCount={15}
+        particleDistances={[90, 10]}
+        particleR={100}
+        initialActiveIndex={0}
+        animationTime={600}
+        timeVariance={300}
+        colors={[1, 2, 3, 1, 2, 3, 1, 4]}
+      />
+    </header>
+  );
+}
